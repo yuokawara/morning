db8567359e01449f7a2181d2f14ace3c070883ec
yuokawara <solbonta@hotmail.com>
2021-02-08 08:56:49 +0900

    morning 20210208

---
+++ study_c/price.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+#define UNIT (30 * 60)
+#define FEE 300
+
+time_t get_caltime(char *tstr);
+
+main()
+{
+    time_t cal_stime, cal_etime;
+    char buff[100];
+    int fee, unit;
+
+    printf("parking start YYYYMMDDHHMMSS : ");
+    scanf("%s", buff);
+    cal_stime = get_caltime(buff);
+    
+    if (cal_stime == -1)
+    {
+        printf("test error\n");
+        return;
+    }
+}
