e5939b0b4141ac65d1805c7691190e268d0156bd
yuokawara <solbonta@hotmail.com>
2021-02-24 08:53:30 +0900

    20210224 wed

---
+++ study_c/main5.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+// int main(void)
+// {
+//     FILE * fp = NULL;
+
+//     fp = fopen("memo.txt", "w");
+
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     FILE * fp1 = NULL;
+//     FILE * fp2 = NULL;
+
+//     fp1 = fopen("memo1.txt", "w");
+//     fp2 = fopen("memo2.txt", "w");
+
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     FILE * fp = NULL;
+
+//     fp = fopen("memo.txt", "w");
+
+//     fclose(fp);
+
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     FILE * fp = NULL;
+//     errno_t no;
+
+//     no = fopen_s(&fp, "memo.txt", "w");
+//     if (no != 0)
+//     {
+//         printf("ERR(%d):オープン失敗", no);
+//     }
+
+//     fclose(fp);
+
+//     return 0;
+// }
+
+int main(void)
+{
+    FILE * fp = NULL;
+}
