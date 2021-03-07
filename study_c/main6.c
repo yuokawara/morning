505ddd2439ba09d8f340ea9dd0cd20e42ca350d0
yuokawara <solbonta@hotmail.com>
2021-03-08 08:39:37 +0900

    20210308 mon

--- study_c/main6.c
+++ study_c/main6.c
@@ -17,4 +17,15 @@ int main(void)
 
     printBalance(hCard1);
     printBalance(hCard2);
+
+    withdrawalMoney(hCard1, 0x1876, 1000);
+    withdrawalMoney(hCard2, 0x1835, 50000);
+
+    printBalance(hCard1);
+    printBalance(hCard2);
+
+    destroyBankAccount(hCard1, 0x1876);
+    destroyBankAccount(hCard2, 0x1835);
+
+    return 0;
 }
