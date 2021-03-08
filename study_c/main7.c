230b44884993f69f810df774d7bc8900373895a4
yuokawara <solbonta@hotmail.com>
2021-03-09 08:26:58 +0900

    20210309 tue

---
+++ study_c/main7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// int main (void)
+// {
+//     int a;
+//     int b = 3;
+//     int add, sub;
+//     double avg;
+//     a = 6;
+//     add = a + b;
+//     sub = a - b;
+//     avg = (a + b) / 2.0;
+
+//     printf("%d + %d = %d", a, b, add);
+
+//     return 0;
+// }
+
+int main (void)
+{
+    int a1 = 2, b1 = 2, c1 = 2;
+}
